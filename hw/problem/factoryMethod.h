24e9dd129afd8b428a11aa6885daa3f687af00d7
Paul Kippes <paul.m.kippes@intel.com>
2017-07-10 21:05:31 -0700

    Refactory factory method problem code

--- hw/problem/factoryMethod.h
+++ hw/problem/factoryMethod.h
@@ -56,7 +56,7 @@ string PVP() {
   return "PVP()";
 }
 
-void clientCode(int fr, int* res, string display, string crypto) {
+void clientCode(int fr, int* res, string display, string /* crypto */) {
   if (display == "DisplayPort") {
     cout << "  Display " << DisplayPort();
   } else if (display == "HDMI") {
@@ -88,73 +88,175 @@ void demo(int seqNo) {
 
 namespace problem {
 
-string DisplayPort() {
-  return "DisplayPort()";
-}
-string HDMI() {
-  return "HDMI()";
-}
-string MIPI() {
-  return "MIPI()";
-}
-string Widi() {
-  return "Widi()";
+// Factory abstract base class
+
+struct Display {
+  virtual ~Display() {
+    DTOR(" ~Display\n", Problem);
+  }
+
+  virtual string format() {
+    return "display-format()";
+  }
+
+  static Display* makeObject(const string& criteria);
+};
+
+// Factory classes
+
+struct DisplayPort : public Display {
+  virtual ~DisplayPort() {
+    DTOR(" ~DisplayPort", Problem);
+  }
+
+  virtual string format() {
+    return "DisplayPort()";
+  }
+};
+
+struct HDMI : public Display {
+  virtual ~HDMI() {
+    DTOR(" ~HDMI", Problem);
+  }
+
+  virtual string format() {
+    return "HDMI()";
+  }
+};
+
+struct MIPI : public Display {
+  virtual ~MIPI() {
+    DTOR(" ~MIPI", Problem);
+  }
+
+  virtual string format() {
+    return "MIPI()";
+  }
+};
+
+struct Widi : public Display {
+  virtual ~Widi() {
+    DTOR(" ~Widi", Problem);
+  }
+
+  virtual string format() {
+    return "Widi()";
+  }
+};
+
+struct HEVC : public Display {
+  virtual ~HEVC() {
+    DTOR(" ~HEVC", Problem);
+  }
+
+  virtual string format() {
+    return "HEVC()";
+  }
+};
+
+// Seam point - add another factory.
+
+Display* Display::makeObject(const string& criteria) {
+  if (criteria == "DisplayPort")
+    return new DisplayPort;
+  else if (criteria == "HDMI")
+    return new HDMI;
+  else if (criteria == "MIPI")
+    return new MIPI;
+  else if (criteria == "Widi")
+    return new Widi;
+  else if (criteria == "HEVC")
+    return new HEVC;
+  // Seam point - insert another criteria.
+
+  return new Display;
 }
 
-string PVP() {
-  return "PVP()";
-}
-string ID1() {
-  return "ID1()";
-}
-string RSA() {
-  return "RSA()";
+// Factory abstract base class
+
+struct Crypto {
+  virtual ~Crypto() {
+    DTOR(" ~Crypto\n", Problem);
+  }
+
+  virtual string protocol() {
+    return "Crypto-protocol()";
+  }
+
+  static Crypto* makeObject(const string& criteria);
+};
+
+// Factory classes
+
+struct PVP : public Crypto {
+  virtual ~PVP() {
+    DTOR(" ~PVP", Problem);
+  }
+
+  virtual string protocol() {
+    return "PVP()";
+  }
+};
+
+struct ID1 : public Crypto {
+  virtual ~ID1() {
+    DTOR(" ~ID1", Problem);
+  }
+
+  virtual string protocol() {
+    return "ID1()";
+  }
+};
+
+struct RSA : public Crypto {
+  virtual ~RSA() {
+    DTOR(" ~RSA", Problem);
+  }
+
+  virtual string protocol() {
+    return "RSA()";
+  }
+};
+
+Crypto* Crypto::makeObject(const string& criteria) {
+  if (criteria == "PVP")
+    return new PVP;
+  else if (criteria == "ID1")
+    return new ID1;
+  else if (criteria == "RSA")
+    return new RSA;
+  // Seam point - insert another criteria.
+
+  return new Crypto;
 }
 
-void clientCode(int fr, int* res, string display, string crypto) {
-  if (display == "DisplayPort") {
-    cout << "  Display " << DisplayPort();
-  } else if (display == "HDMI") {
-    cout << "  Display " << HDMI();
-  } else if (display == "MIPI") {
-    cout << "  Display " << MIPI();
-  } else if (display == "Widi") {
-    cout << "  Display " << Widi();
-  }
-  // Seam point - insert another display format.
-  else {
-    cout << "  Display "
-         << "display format()";
-  }
+void clientCode(int fr, int* res, Display* display, Crypto* crypto) {
+  cout << "Display " << display->format();
 
   cout << " at " << fr << " frames/sec";
   cout << " with [" << res[0] << ", " << res[1] << "]";
 
-  if (crypto == "PVP") {
-    cout << " via " << PVP() << ".\n";
-  } else if (crypto == "ID1") {
-    cout << " via " << ID1() << ".\n";
-  } else if (crypto == "RSA") {
-    cout << " via " << RSA() << ".\n";
-  }
-  // Seam point - insert another crypto protocol.
-  else {
-    cout << " via "
-         << "crypto protocol()"
-         << ".\n";
-  }
+  cout << " via " << crypto->protocol() << ".\n";
 }
 
 void demo(int seqNo) {
   cout << seqNo << ") << factory_method::homework::problem::demo() >>\n";
   framerate = 60;
   res[0] = 1920, res[1] = 1080;
-  string displays[] = {"DisplayPort", "HDMI", "MIPI", "Widi"};
+  string displays[] = {"DisplayPort", "HDMI", "MIPI", "Widi", "HEVC"};
   string cryptos[] = {"PVP", "ID1", "RSA"};
   for (size_t i = 0; i < COUNT(displays); i++) {
+    Display* display = Display::makeObject(displays[i]);
+
     for (size_t j = 0; j < COUNT(cryptos); j++) {
-      clientCode(framerate, res, displays[i], cryptos[j]);
+      Crypto* crypto = Crypto::makeObject(cryptos[j]);
+
+      clientCode(framerate, res, display, crypto);
+
+      delete crypto;
     }
+
+    delete display;
   }
 }
 
