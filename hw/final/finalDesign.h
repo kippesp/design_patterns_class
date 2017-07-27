f56b3f6281a717a2a3c07dbcdccac2904b919c29
Paul Kippes <paul.m.kippes@intel.com>
2017-07-27 13:18:26 -0700

    apply clang-format

--- hw/final/finalDesign.h
+++ hw/final/finalDesign.h
@@ -1,3 +1,4 @@
+
 /*
  * finalDesign.h
  *
@@ -10,17 +11,21 @@
 
 #include <cassert>
 
-namespace final_design {
+namespace final_design
+{
 
-const bool on = true;  // Useful macro to turn dtor instrumentation on/off.
-#define DTORF(x) \
-  if (on) {      \
-    cout << x;   \
+const bool on = true; // Useful macro to turn dtor instrumentation on/off.
+#define DTORF(x)                                                               \
+  if (on)                                                                      \
+  {                                                                            \
+    cout << x;                                                                 \
   }
 
-namespace legacy_classes {  // Can't change these.
+namespace legacy_classes
+{ // Can't change these.
 
-int cycleTime_sec(string metal, int volume_cc) {
+int cycleTime_sec(string metal, int volume_cc)
+{
   int heatCapacityAdjustment = 0;
 
   if (metal == "aluminum")
@@ -36,41 +41,43 @@ const int teardownAvg_min = 54;
 const float rejectRateLow_pcnt = 0.2;
 const float rejectRateHigh_pcnt = 1.1;
 
-class CleanABS {
- public:
-  ~CleanABS() {
-    DTORF("~legacy_classes::CleanABS ");
-  }
-  void clean() {
+class CleanABS
+{
+public:
+  ~CleanABS() { DTORF("~legacy_classes::CleanABS "); }
+  void clean()
+  {
     cout << "    Clean ABS mold: soak in alcohol, rinse with water, dry.\n";
   }
 };
 
-class CleanPolys {
- public:
-  ~CleanPolys() {
-    DTORF("~legacy_classes::CleanPolys ");
-  }
-  void prepForReuse() {
+class CleanPolys
+{
+public:
+  ~CleanPolys() { DTORF("~legacy_classes::CleanPolys "); }
+  void prepForReuse()
+  {
     cout << "    Clean Poly mold: rinse with acetone, dry.\n";
   }
 };
 
-class PETCleanup {
- public:
-  ~PETCleanup() {
-    DTORF("~legacy_classes::PETCleanup ");
-  }
-  void carbonize() {  // Use only on stainless steel.
+class PETCleanup
+{
+public:
+  ~PETCleanup() { DTORF("~legacy_classes::PETCleanup "); }
+  void carbonize()
+  { // Use only on stainless steel.
     cout << "    Clean PET steel mold: heat to 895 C.\n";
   }
-  void purify() {  // Use only on aluminum.
+  void purify()
+  { // Use only on aluminum.
     cout << "    Clean PET aluminum mold: rinse with soap and water, dry.\n";
   }
 };
 
 void defaulting(map<string, string>& order, const string& option,
-                string def = "simulation") {
+                string def = "simulation")
+{
   cout << "  <>Unknown " << option << " |";
   cout << order[option] << "|";
   order[option] = def;
@@ -78,22 +85,26 @@ void defaulting(map<string, string>& order, const string& option,
 }
 }
 
-namespace adapter {  // DP 2.
+namespace adapter
+{ // DP 2.
 
 // Seam point - add another interface.
 }
 
-namespace strategy {  // DP 1.
+namespace strategy
+{ // DP 1.
 
 // Seam point - add another algorithm.
 }
 
-namespace observer {  // DP 5.
+namespace observer
+{ // DP 5.
 
 // Seam point - add another listener.
 }
 
-namespace abstract_factory {  // DP 9.
+namespace abstract_factory
+{ // DP 9.
 
 // Seam point - add another type 1.
 // Seam point - add another type 2.
@@ -103,50 +114,59 @@ namespace abstract_factory {  // DP 9.
 // Seam point - add another family.
 }
 
-namespace bridge {  // DP 8.
+namespace bridge
+{ // DP 8.
 
 // Seam Point - add another implementation.
 // Seam Point - add another abstraction.
 }
 
-namespace chain_of_resp {  // DP 7.
+namespace chain_of_resp
+{ // DP 7.
 
 // Seam points - add another responder.
 }
 
-namespace decorator {  // DP 6.
+namespace decorator
+{ // DP 6.
 
 // Seam point - add another option.
 }
 
-namespace factory_method {  // DP 4.
+namespace factory_method
+{ // DP 4.
 
 // Seam point - add another class.
 }
 
-namespace template_method {  // DP 3.
+namespace template_method
+{ // DP 3.
 
 // Seam point - add another polymorphic step.
 // Seam point - add another constant step.
 // Seam point - convert a constant step into a polymorphic step.
 }
 
-void process(map<string, string>& /* order */) {
+void process(map<string, string>& /* order */)
+{
   // Fill in the namespaces above with your design pattern class hierarchies.
   // Call your order processing class from here <myProcess>->run(order);
 }
 
-pair<string, string> parse(string line) {
+pair<string, string> parse(string line)
+{
   char key[83];
-  char val[83] = {0};
+  char val[83] = { 0 };
 
   sscanf(line.c_str(), "%s", key);
 
   const char* equalSign = strchr(line.c_str(), '=');
   string value = "";
-  if (equalSign) {  // tags = sam i am
+  if (equalSign)
+  { // tags = sam i am
     const char* nextToken = equalSign += 2;
-    while (nextToken) {
+    while (nextToken)
+    {
       sscanf(nextToken, "%s", val);
       value += val;
       nextToken = strchr(nextToken + 1, ' ');
@@ -158,19 +178,24 @@ pair<string, string> parse(string line) {
   return make_pair(key, value);
 }
 
-map<string, string> getCompleteOrder(FILE* orderFilePtr) {
+map<string, string> getCompleteOrder(FILE* orderFilePtr)
+{
   map<string, string> order;
   char line[80 + 3];
   pair<string, string> keyValue;
 
-  while (fgets(line, 80, orderFilePtr)) {
+  while (fgets(line, 80, orderFilePtr))
+  {
     cout << line;
-    if (strstr(line, "endOfOrder")) {
+    if (strstr(line, "endOfOrder"))
+    {
       return (order);
-    } else {
+    }
+    else
+    {
       keyValue = parse(line);
       string key = keyValue.first;
-      if (strchr(key.c_str(), ':'))  // Skip left justified order number.
+      if (strchr(key.c_str(), ':')) // Skip left justified order number.
         continue;
       string value = keyValue.second;
       order[key] = value;
@@ -180,19 +205,22 @@ map<string, string> getCompleteOrder(FILE* orderFilePtr) {
   return order;
 }
 
-void demo(const string& orderFile) {
+void demo(const string& orderFile)
+{
   cout << "<<< final design >>>\n";
 
   FILE* orderFilePtr = fopen(orderFile.c_str(), "r");
   assert(orderFilePtr != NULL);
 
-  for (;;) {  // Get and process each order.
+  for (;;)
+  { // Get and process each order.
     map<string, string> order = getCompleteOrder(orderFilePtr);
-    if (order.size() == 0) break;
+    if (order.size() == 0)
+      break;
     process(order);
   }
 }
 
-}  // final_design
+} // final_design
 
 #endif /* FINAL_FINAL_DESIGN_H_ */
