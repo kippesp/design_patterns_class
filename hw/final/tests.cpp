c1c868a04d13f575f45758c870bb77dcebf503f9
Paul Kippes <paul.m.kippes@intel.com>
2017-07-30 22:11:08 -0700

    basic test poc

--- hw/final/tests.cpp
+++ hw/final/tests.cpp
@@ -8,3 +8,38 @@ using namespace std;
 
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+
+stringstream captured_stdout;
+streambuf* sbuf;
+
+void capture_on()
+{
+  captured_stdout.str(std::string());
+  sbuf = cout.rdbuf();
+  cout.rdbuf(captured_stdout.rdbuf());
+}
+
+void capture_off()
+{
+  // output
+  cout.rdbuf(sbuf);
+}
+
+TEST_CASE("This is a test", "something")
+{
+
+  final_design::legacy_classes::CleanABS c;
+  final_design::legacy_classes::CleanPolys d;
+
+  capture_on();
+  c.clean();
+  REQUIRE(captured_stdout.str() ==
+          "    Clean ABS mold: soak in alcohol, rinse with water, dry.\n");
+  capture_off();
+
+  capture_on();
+  d.prepForReuse();
+  REQUIRE(captured_stdout.str() ==
+          "    Clean Poly mold: rinse with acetone, dry.\n");
+  capture_off();
+}
